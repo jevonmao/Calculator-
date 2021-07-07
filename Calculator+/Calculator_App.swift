2f087eabbe1707dc1c25520f755899e54473c614
Jevon Mao <woodburyjevonmao@gmail.com>
2021-07-07 16:37:39 -0700

    Initial Commit

---
+++ Calculator+/Calculator_App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_App.swift
+//  Calculator+
+//
+//  Created by Jevon Mao on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator+/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator+
+//
+//  Created by Jevon Mao on 7/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
